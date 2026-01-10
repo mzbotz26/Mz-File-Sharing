03e54ecfc63e49f374cf90c770b591c2da4abf1f
mzbotz26 <mrsam2395@gmail.com>
2026-01-11 00:12:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "MzFileShare")
 #force sub channel id, if you want enable force sub
 FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1001956378045"))
 FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002265223561"))
-FORCESUB_CHANNEL3 = int(os.environ.get("FORCESUB_CHANNEL3", "-1001502695531"))
+FORCESUB_CHANNEL3 = int(os.environ.get("FORCESUB_CHANNEL3", "-1001931113198"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
