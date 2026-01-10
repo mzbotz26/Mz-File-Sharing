7240e90787880ac1fc8ac0fb376ec79b84dc4654
mzbotz26 <mrsam2395@gmail.com>
2026-01-11 02:25:51 +0530

    Update reset _catalog.py

--- plugins/reset _catalog.py
+++ plugins/reset _catalog.py
@@ -5,5 +5,11 @@ from database.database import series_catalog
 
 @Bot.on_message(filters.command("reset_catalog") & filters.private & filters.user(ADMINS))
 async def reset_catalog(client, message):
+
     await series_catalog.delete_many({})
-    await message.reply("✅ series_catalog collection successfully cleared!")
+
+    await message.reply(
+        "✅ <b>Series catalog successfully reset!</b>\n\n"
+        "अब सभी series / episodes fresh start से auto group होंगे.",
+        parse_mode="html"
+    )
