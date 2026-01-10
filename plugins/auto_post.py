52464ac48c0eb1331b9edf59001f9c918648504c
mzbotz26 <mrsam2395@gmail.com>
2026-01-10 14:26:13 +0530

    Create auto_post.py

---
+++ plugins/auto_post.py
@@ -0,0 +1,16 @@
+from bot import Bot
+from pyrogram import filters
+from config import CHANNEL_ID
+
+POST_CHANNEL = -100XXXXXXXXXX   # <-- apna post channel id daalo
+
+@Bot.on_message(filters.chat(CHANNEL_ID))
+async def auto_post(client, message):
+
+    if not (message.document or message.video or message.audio):
+        return
+
+    try:
+        await message.copy(POST_CHANNEL)
+    except Exception as e:
+        print("AUTO POST ERROR:", e)
