64905e9454d20791fbc96ff1f79a34439675e1ac
mzbotz26 <mrsam2395@gmail.com>
2026-01-11 09:35:26 +0530

    Update reset _catalog.py

--- plugins/reset _catalog.py
+++ plugins/reset _catalog.py
@@ -5,9 +5,5 @@ from database.database import series_catalog
 
 @Bot.on_message(filters.command("reset_catalog") & filters.private & filters.user(ADMINS))
 async def reset_catalog(client, message):
-
     await series_catalog.delete_many({})
-
-    await message.reply(
-        "✅ <b>Catalog reset successfully!</b>\n\nAll movie & series posts are cleared.\nAuto post will start fresh now."
-    )
+    await message.reply("✅ Series catalog reset successfully.")
