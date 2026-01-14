444230ab179743d2b9ad5133a257048d356364c9
mzbotz26 <mrsam2395@gmail.com>
2026-01-14 14:30:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,9 +85,9 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ultroid_official")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Mzbotz")
         self.LOGGER(__name__).info(f""" \n\n       
-(っ◔◡◔)っ ♥ ULTROIDOFFICIAL ♥
+(っ◔◡◔)っ ♥ MZBOTZ ♥
 ░╚════╝░░╚════╝░╚═════╝░╚══════╝
                                           """)
         self.username = usr_bot_me.username
